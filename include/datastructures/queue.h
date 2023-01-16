cc62c5525cc2d915fe5e660be22d9f50b0a805f9
Tim Reinhart <tim@MacBook-Pro-76.home>
2023-01-16 14:19:36 +0100

    added first implementation of queue

--- include/datastructures/queue.h
+++ include/datastructures/queue.h
@@ -1,1 +1,34 @@
-class Queue {};
+// Implementation of a FIFO queue
+#ifndef BASIC_ALGOS_DS_QUEUE_H
+#define BASIC_ALGOS_DS_QUEUE_H
+
+#include <optional>
+
+template <typename T>
+class Queue {
+   public:
+    Queue() = default;
+    Queue(std::initializer_list<T> const& list) : vector(list) {}
+    Queue(Queue const& rhs) = default;
+
+    Queue& operator=(Queue const& rhs) = default;
+
+    void enqueue(T val) { vector.push_back(val); }
+    std::optional<T> dequeue() {
+        if (empty()) {
+            return std::nullopt;
+        }
+        T last = vector[size() - 1];
+        vector.pop_back();
+        return last;
+    }
+
+    T front() { return vector.front(); }
+    T back() { return vector.back(); }
+    bool empty() { return vector.size() == 0; }
+    size_t size() { return vector.size(); }
+
+   private:
+    std::vector<T> vector;
+};
+#endif  // BASIC_ALGOS_DS_QUEUE_H
--- test/queue_test.cpp
+++ test/queue_test.cpp
@@ -1,2 +1,35 @@
-#include "gtest/gtest.h"
 #include "queue.h"
+
+#include "gtest/gtest.h"
+
+TEST(queue_tests, enqueue) {
+    Queue<int> q;
+    ASSERT_TRUE(q.empty());
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+    ASSERT_TRUE(q.size() == 3);
+}
+
+TEST(queue_tests, dequeue) {
+    Queue<float> q{1.f, 2.f, 3.f};
+    ASSERT_TRUE(q.size() == 3);
+
+    auto const& last1 = q.dequeue();
+    ASSERT_TRUE(last1);
+    ASSERT_FLOAT_EQ(*last1, 3.f);
+    ASSERT_TRUE(q.size() == 2);
+
+    auto const& last2 = q.dequeue();
+    ASSERT_TRUE(last2);
+    ASSERT_FLOAT_EQ(*last2, 2.f);
+    ASSERT_TRUE(q.size() == 1);
+
+    auto const& last3 = q.dequeue();
+    ASSERT_TRUE(last3);
+    ASSERT_FLOAT_EQ(*last3, 1.f);
+    ASSERT_TRUE(q.size() == 0);
+
+    auto const& last_null = q.dequeue();
+    ASSERT_FALSE(last_null);
+}
