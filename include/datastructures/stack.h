86ad3539989c894d4b30c1072d96d553c8ec8974
Tim Reinhart <tim@macbook-pro.home>
2023-01-24 12:44:53 +0100

    adds stack including test

---
+++ include/datastructures/stack.h
@@ -0,0 +1,71 @@
+#ifndef BASIC_ALGOS_DS_STACK_H
+#define BASIC_ALGOS_DS_STACK_H
+
+#include <initializer_list>
+#include <iostream>
+#include <optional>
+
+template <typename T>
+class Stack {
+   public:
+    Stack() = default;
+
+    explicit Stack(T val) { head_ = tail_ = new Node(val); }
+
+    explicit Stack(std::initializer_list<T> list) {
+        for (T const& elem : list) {
+            insert(elem);
+        }
+    }
+
+    void insert(T val) {
+        Node* new_node = new Node(val);
+        if (!head_) {
+            head_ = tail_ = new_node;
+            return;
+        }
+        tail_->next = new_node;
+        tail_ = new_node;
+    }
+
+    std::optional<T> pop() {
+        if (!head_) {
+            return std::nullopt;
+        }
+        Node* node = head_;
+        Node* prev = nullptr;
+        while (node != tail_) {
+            prev = node;
+            node = node->next;
+        }
+        T val = tail_->value;
+        tail_ = prev;
+        tail_->next = nullptr;
+        return val;
+    }
+
+    void print() const {
+        Node* node = head_;
+        while (node) {
+            std::cout << node->value;
+            if (node->next) {
+                std::cout << " -> ";
+            }
+            node = node->next;
+        }
+        std::cout << "\n";
+    }
+
+   private:
+    struct Node {
+        T value;
+        Node* next = nullptr;
+
+        Node(T val) : value(val) {}
+    };
+
+    Node* head_ = nullptr;
+    Node* tail_ = nullptr;
+};
+
+#endif  // BASIC_ALGOS_DS_STACK_H
---
+++ test/stack_test.cpp
@@ -0,0 +1,17 @@
+#include "stack.h"
+
+#include "gtest/gtest.h"
+
+TEST(stack_test, insert_and_pop) {
+    Stack<int> s;
+    s.insert(1);
+    s.insert(2);
+    s.insert(3);
+    s.insert(4);
+    s.print();
+    EXPECT_EQ(s.pop(), 4);
+    EXPECT_EQ(s.pop(), 3);
+    EXPECT_EQ(s.pop(), 2);
+    EXPECT_EQ(s.pop(), 1);
+    EXPECT_FALSE(s.pop());
+}
